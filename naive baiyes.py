fbe8f74ac74b81e8c59031bfbab6ca9aa8a36eed
puji29 <66867777+puji29@users.noreply.github.com>
2023-06-13 20:32:07 +0700

    create web deteksi using pytjon

--- naive baiyes.py
+++ naive baiyes.py
@@ -5,8 +5,8 @@ import joblib
 
 df = pd.read_csv("D:/SKRIPSI/fitur glcm 224.csv")
 
-X = df.iloc[:,0:5]
-y = df.iloc[:,5]
+X = df.iloc[:,0:4]
+y = df.iloc[:,4]
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
